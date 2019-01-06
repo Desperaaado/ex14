c89d13f44343c6a2a30c070a99622b6d88524a7a
murphian <murphianx@gmail.com>
2019-01-06 17:05:41 +0800

    add func: push() pop() dump() and their test function

--- ex14/ex14.py
+++ ex14/ex14.py
@@ -5,15 +5,61 @@ class DLLNode(object):
         self.prev = prev
 
     def __repr__(self):
-        return f"{self.prev} <- {self.value} -> {self.next}"
+        # return f"{self.prev} <- {self.value} -> {self.next}"
+        return f"=={self.value}=="
 
 class DLList(object):
-    def __init__(self, begin, end):
-        self.begin = begin
-        self.end = end
+    def __init__(self):
+        self.begin = None
+        self.end = None
 
     def push(self, obj):
-        pass
+        if not self.begin:
+            self.begin = DLLNode(obj, None, None)
+            self.end = self.begin
+        elif self.begin == self.end:
+            node = DLLNode(obj, None, self.begin)
+            self.end = node
+            self.begin.next = node
+        else:
+            node = self.end
+            node.next = DLLNode(obj, None, self.end)
+            self.end = node.next
 
     def pop(self):
+        if not self.begin:
+            print('Empty List!')
+            result = None
+        elif self.begin == self.end:
+            result = self.begin.value
+            self.begin = None
+            self.end = None
+        else:
+            result = self.end.value
+            node = self.end.prev
+            node.next = None
+            self.end = node
+
+        return result
+        
+    def dump(self, mode='normal'):
+        the_list = []
+
+        if mode == 'normal':
+            node = self.begin
+            
+            if not node:
+                print('Empty List!')
+                return None
+
+            the_list.append(node.value)
+
+            while node.next:
+                node = node.next
+                the_list.append(node.value)
+
+        print('list: ', the_list)
+        return the_list
+
+    def remove(self, obj):
         pass
--- tests/ex14_tests.py
+++ tests/ex14_tests.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-import ex14
+from ex14.ex14 import *
 
 def setup():
     print("SETUP!")
@@ -7,5 +7,22 @@ def setup():
 def teardown():
     print("TEAR DOWN!")
 
-def test_basic():
-    print("I RAN!")
+def test_push():
+    colors = DLList()
+    colors.push('apple')
+    colors.push('banana')
+    colors.push('canada')
+    colors.push('duck')
+
+def test_pop():
+    colors = DLList()
+    assert colors.pop() == None
+    colors.push('apple')
+    assert colors.pop() == 'apple'
+    colors.push('banana')
+    colors.push('canada')
+    assert colors.pop() == 'canada'
+    colors.push('duck')
+    assert colors.pop() == 'duck'
+    colors.dump()
+
