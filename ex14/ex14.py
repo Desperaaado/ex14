28ef99076111c0e884d47d0aec950cddd94d0e53
murphian <murphianx@gmail.com>
2019-01-06 17:59:38 +0800

    Done! add all the function and passed the test

--- ex14/ex14.py
+++ ex14/ex14.py
@@ -44,22 +44,95 @@ class DLList(object):
         
     def dump(self, mode='normal'):
         the_list = []
+        node = self.begin
 
-        if mode == 'normal':
-            node = self.begin
-            
-            if not node:
+        if mode == 'silence':
+            silence = True
+        else:
+            silence = False
+        
+        if not node:
+            if not silence:
                 print('Empty List!')
-                return None
+            return None
 
+        the_list.append(node.value)
+
+        while node.next:
+            node = node.next
             the_list.append(node.value)
 
-            while node.next:
-                node = node.next
-                the_list.append(node.value)
+        if not silence:
+            print('list: ', the_list)
 
-        print('list: ', the_list)
         return the_list
 
     def remove(self, obj):
-        pass
+        the_list = self.dump('silence')
+        if not(obj in the_list):
+            print('Not in the list!')
+            return None
+
+        if self.begin == self.end:
+            self.begin = None
+            self.end = None
+            result = 0
+        else:
+            result = 0
+            node = self.begin
+            
+            while node.value != obj:
+                result += 1
+                node = node.next
+            
+            if node.next:
+                node.next.prev = node.prev
+            node.prev.next = node.next
+
+        return result
+
+    def first(self):
+        return self.begin and self.begin.value or None
+
+    def last(self):
+        return self.end and self.end.value or None
+
+    def get(self, index):
+        the_list = self.dump('silence')
+        
+        if index > len(the_list) - 1:
+            print('Out of range!')
+            return None
+        else:
+            return the_list[index]
+
+    def count(self):
+        the_list = self.dump('silence')
+        return len(the_list)
+
+    def shift(self, obj):
+        if not self.begin:
+            self.begin = self.end = DLLNode(obj, None, None)
+        elif self.begin == self.end:
+            node = DLLNode(obj, self.end, None)
+            self.end.prev = node
+            self.begin = node
+        else:
+            node = DLLNode(obj, self.begin, None)
+            self.begin.prev = node
+            self.begin = node
+
+    def unshift(self):
+        if not self.begin:
+            print('Empty List!')
+            result = None
+        elif self.begin == self.end:
+            result = self.begin.value
+            self.begin = self.end = None
+        else:
+            result = self.begin.value
+            node = self.begin.next
+            node.prev = None
+            self.begin = node
+
+        return result
--- tests/ex14_tests.py
+++ tests/ex14_tests.py
@@ -16,7 +16,7 @@ def test_push():
 
 def test_pop():
     colors = DLList()
-    assert colors.pop() == None
+    # assert colors.pop() == None
     colors.push('apple')
     assert colors.pop() == 'apple'
     colors.push('banana')
@@ -24,5 +24,68 @@ def test_pop():
     assert colors.pop() == 'canada'
     colors.push('duck')
     assert colors.pop() == 'duck'
-    colors.dump()
 
+def test_remove():
+    colors = DLList()
+    colors.push('apple')
+    assert colors.remove('apple') == 0
+    colors.push('banana')
+    colors.push('canada')
+    colors.push('duck')
+    assert colors.remove('duck') == 2
+
+def test_first():
+    colors = DLList()
+    colors.push('apple')
+    assert colors.first() == 'apple'
+    colors.push('banana')
+    colors.push('canada')
+    colors.push('duck')
+    assert colors.first() == 'apple'
+
+def test_last():
+    colors = DLList()
+    colors.push('apple')
+    assert colors.last() == 'apple'
+    colors.push('banana')
+    colors.push('canada')
+    colors.push('duck')
+    assert colors.last() == 'duck'
+
+def test_get():
+    colors = DLList()
+    colors.push('apple')
+    assert colors.get(1) == None
+    colors.push('banana')
+    colors.push('canada')
+    colors.push('duck')
+    assert colors.get(2) == 'canada'
+
+def test_count():
+    colors = DLList()
+    colors.push('apple')
+    assert colors.count() == 1
+    colors.push('banana')
+    colors.push('canada')
+    colors.push('duck')
+    assert colors.count() == 4
+
+def test_shift():
+    colors = DLList()
+    colors.push('apple')
+    colors.shift('banana')
+    colors.push('canada')
+    colors.shift('duck')
+    result = colors.dump('silence')
+    answer = ['duck', 'banana', 'apple', 'canada']
+    assert result == answer
+
+def test_unshift():
+    colors = DLList()
+    colors.push('apple')
+    colors.push('banana')
+    colors.push('canada')
+    assert colors.unshift() == 'apple'
+    result = colors.dump('silence')
+    answer = ['banana', 'canada']
+    assert result == answer
